e2086b009a779141b08b00404e00b88aedbdf81e
Carlos Veizaga <carlosveizaga.jcvc@gmail.com>
2025-12-03 09:08:22 -0400

    fix: remove redundant mousemove handler to improve performance

--- src/components/classic/SkillSection.vue
+++ src/components/classic/SkillSection.vue
@@ -144,17 +144,6 @@
 <script setup>
 import SkillBentoItem from './SkillBentoItem.vue'
 import TechRiver from './TechRiver.vue'
-
-const handleMouseMove = (e) => {
-  const cards = document.querySelectorAll('.group')
-  for (const card of cards) {
-    const rect = card.getBoundingClientRect()
-    const x = e.clientX - rect.left
-    const y = e.clientY - rect.top
-    card.style.setProperty('--mouse-x', `${x}px`)
-    card.style.setProperty('--mouse-y', `${y}px`)
-  }
-}
 </script>
 
 <style scoped>
